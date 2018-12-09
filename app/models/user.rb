00eef2b3f8e28a07553e4f76c9cf566191e48d9a
Dziad <18483718+Dziadzlasu@users.noreply.github.com>
2018-12-09 16:31:53 +0100

    Add and configure devise for user sign in and log in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,39 @@
 class User < ApplicationRecord
+  attr_writer :login
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates_presence_of :email, :first_name, :last_name
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
 
   has_many :wishes
 
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
   end
 end
