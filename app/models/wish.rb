c257b2548f7d5f9adaf1b10de533d053de92a73b
Dziad <18483718+Dziadzlasu@users.noreply.github.com>
2018-12-17 20:31:28 +0100

    add wish expiration service

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,8 +1,9 @@
 class Wish < ApplicationRecord
-  belongs_to :user, optional: true # optional is temporary, until devise is set up
+  belongs_to :user
 
   validates_presence_of :description
 
   scope :pending, -> { where(status: 'pending') }
   scope :fulfilled, -> { where(status: 'completed') }
+  scope :expired, -> { where(status: 'expired') }
 end
